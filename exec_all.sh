a44918081c3a13fcfede4f174b45edf651b2855c
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2017-05-27 10:47:19 -0300

    readme and execute

---
+++ exec_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+./generate_base_acc idclassimg.dat
+./generate_base_bas idclassimg.dat
+./generate_base_bic idclassimg.dat
+./generate_base_cs idclassimg.dat
+./generate_base_csd idclassimg.dat
+./generate_base_fourier idclassimg.dat
+./generate_base_gch idclassimg.dat
+./generate_base_htd idclassimg.dat
+./generate_base_jac idclassimg.dat
+./generate_base_las idclassimg.dat
+./generate_base_lbp idclassimg.dat
+./generate_base_lucolor idclassimg.dat
+./generate_base_momentsinvariant idclassimg.dat
+./generate_base_msfractal idclassimg.dat
+./generate_base_sasi idclassimg.dat
+./generate_base_sid idclassimg.dat
+./generate_base_sms idclassimg.dat
+./generate_base_ss idclassimg.dat
+./generate_base_ts idclassimg.dat
+./generate_base_tsdiz idclassimg.dat
